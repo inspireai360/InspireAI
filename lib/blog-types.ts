eabfbcec8d703ffbec1251930b4278cf1e4e0f31
inspireai360 <inspireaiagency.contact@gmail.com>
2026-05-31 01:28:00 +0200

    feat: blog types

---
+++ lib/blog-types.ts
@@ -0,0 +1,13 @@
+export interface BlogPost {
+  slug: string
+  title: string
+  metaTitle: string
+  metaDescription: string
+  excerpt: string
+  date: string
+  readTime: number
+  category: string
+  keywords: string[]
+  relatedService?: string
+  relatedServiceUrl?: string
+}
